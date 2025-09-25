750eb4b1f7d6cfe3b3d4514bc3764ac722f4869a
Sehab Ahmed <sehabahmed50100@gmail.com>
2025-09-25 09:57:07 +0200

    fix: resolve parsedBody json data configuration

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 ## setup body parser middleware whether if not put json data inside body.
+
+## most importantly, this tool is created for mongoose friendly. if someone need boilerplate node-express initial kit. here we are.
--- src/app/modules/testModule/test.route.ts
+++ src/app/modules/testModule/test.route.ts
@@ -2,10 +2,16 @@ import express from "express";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { TestValidations } from "./test.validaiton";
 import { TestControllers } from "./test.controller";
+import { parseBody } from "../../middlewares/bodyParser";
 
 const router = express.Router();
 
-router.post("/create-test", validateRequest(TestValidations.createTestSchema), TestControllers.createTest);
+router.post(
+  "/create-test",
+  parseBody,
+  validateRequest(TestValidations.createTestSchema),
+  TestControllers.createTest
+);
 
 router.get("/", TestControllers.getAllItems);
 
